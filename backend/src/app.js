a45a951974f5ce51d58429c59424c6dd997d48cf
Aviraj Gite <giteaviraj155@gmail.com>
2026-09-14 00:00:34 +0530

    Created Express Server

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,15 @@
 //create server
 
 const express = require("express");
+const cokkieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
+const authRoutes
 
 const app = express();
+
+app.use(cookieParser())
+
+app.use(express.json())
 
 app.get("/",(req,res)=>{
     res.send("Pappa is back")
---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,40 @@
+const userModel = require("../models/user.model");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken")
+
+async function registerUser(req, res) {
+  const { fullName, email, password } = req.body;
+
+  const isUserAlreadyExist = await userModel.findOne({ email });
+
+  if (isUserAlreadyExist){;
+  return res.status(400).json({
+    message: "User already exists",
+  });
+}
+
+
+const hashedPassword = await bcrypt.hash(password,10);
+
+const user = await userModel.create({
+    fullName,
+    email,
+    password:hashedPassword
+})
+
+const token = jwt.sign({
+    id:user._id,
+},"dbe686a76a8fbcfc738f823614c6a03554f9ac29")
+res.cookie("token",token);
+
+res.status(201).json({
+    message:"User registered Successfully",
+    user:{
+        _id:user._id,
+        email:user.email,
+        fullName: user.fullName
+    }
+
+})
+
+}
