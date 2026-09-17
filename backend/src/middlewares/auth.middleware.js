afbc90b9f558b5570cf4a574d94186e99dfeb042
Aviraj Gite <giteaviraj155@gmail.com>
2026-09-17 20:17:25 +0530

    added cloud srorage services

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -1,34 +1,28 @@
 const foodPartnerModel = require("../models/foodpartener.model");
 const jwt = require("jsonwebtoken");
 
+async function authFoodPartnerMiddleware(req, res, next) {
+  const token = req.cookies.foodPartnerToken;
 
-async function authFoodPartnerMiddleware(req,res,next){
-        
-    const token = req.cookies.foodPartnerToken;
+  if (!token) {
+    return res.status(401).json({
+      message: "Unauthorized access",
+    });
+  }
 
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+    const foodPartner = await foodPartnerModel.findById(decoded.id);
 
-    if(!token){
-        return res.status(401).json({
-            message:"Unauthorized access"
-        })
-    }
+    req.foodPartner = foodPartner;
 
-    try{
-        const decoded = jwt.verify(token,process.env.JWT_SECRET);
-
-        const foodPartner = await foodPartnerModel.findById(decoded.id);
-
-        req.foodPartner = foodPartner
-
-        next()
-
-    }catch(err){
-        return res.status(401).json({
-            message:"Invaid Token"
-        })
-    }
+    next();
+  } catch (err) {
+    return res.status(401).json({
+      message: "Invaid Token",
+    });
+  }
 }
 
-
-module.exports={authFoodPartnerMiddleware}
+module.exports = { authFoodPartnerMiddleware };
